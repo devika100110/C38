d5ac260f146272488f6f60f7d7aae423aea882a3
devika100110 <77834219+devika100110@users.noreply.github.com>
2021-04-23 15:13:15 +0530

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -42,10 +42,11 @@ class Game {
             fill ("red");
         else 
             fill ("black");
-      }
+      
       display_position=display_position+20;
       textSize(15);
       text(allPlayers[plr].name+": "+allPlayers[plr].distance,120,display_position);
+      }
     }
     if(keyIsDown(UP_ARROW) && player.index!== null){
       player.distance=player.distance+50;
